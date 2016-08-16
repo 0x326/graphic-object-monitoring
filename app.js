399e942a4efc223fb57c6528af49452a26ce2a3a
John Meyer <jmm2016@ymail.com>
2016-08-16 09:17:08 -0400

    Remove code pertaining to 0x326/waypoint-navigation

--- app.js
+++ app.js
@@ -4,20 +4,8 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-// Set-up drone controller
-const WaypointNavigator = require("./");
-var waypointNavigation = new WaypointNavigator();
-
 server.listen(3000);
 app.use(express.static('public'));
 
-io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
-});
-
-waypointNavigation.mission().client().on('navdata', function (data) {
-    io.emit('navdata', data);
-});
+// Emit this object on a regular basis
+io.emit('checkObject', data);
